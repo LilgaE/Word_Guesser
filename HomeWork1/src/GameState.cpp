0d63caf0262bf1565224b8e8db87e121c94c1bfd
LilgaE <31768184+LilgaE@users.noreply.github.com>
2021-02-15 09:25:02 -0600

    2/14/2021 progress

--- HomeWork1/src/GameState.cpp
+++ HomeWork1/src/GameState.cpp
@@ -10,22 +10,22 @@ using namespace std;
 #include "GameState.h"
 
 
-GameState::GameState() {
-		_Word = w;
-		_Guess = g;
-		_numGuess = 0;
-	// TODO Auto-generated constructor stub
-
-}
-
-void setWord(string w){
-	_Word = w;
-
-}
-void userGuess(string g){
-
-
-}
-void checkGuess(string g){
-
-}
+GameState::GameState() {}
+//		_Word = w;
+//		_Guess = g;
+//		_numGuess = 0;
+//	// TODO Auto-generated constructor stub
+//
+//}
+//
+//void GameState:setWord(string w){
+//	_Word = w;
+//
+//}
+//void GameState:userGuess(string g){
+//
+//
+//}
+//void GameState:checkGuess(string g){
+//
+//}
--- HomeWork1/src/HomeWork1.cpp
+++ HomeWork1/src/HomeWork1.cpp
@@ -8,59 +8,63 @@
 
 #include <iostream>
 #include <string>
-#include <vector>
 #include <fstream>
 using namespace std;
-#include "GameState.h"
+//#include "GameState.h"
 #include "Player.h"
 #include "Dictionary.h"
 
-int main() {
-	int RunTest = 0;
 	int Kplaying = 1;
-	Dictionary Dict;
-	Player Play;
-	GameState GS;
-	string openFile;
+	string test = "";
+	Dictionary Dict("");
+	Player Play("",0,0);
+	//GameState GS;
+	string openFile = "";
 	string user = "test";
 
 
-
-
-
-	cout << "Welcome to the wild word guessing game!" << endl << "Please enter 1 to use the premade dictionary." << endl << "Please enter 2 to create your own dictionary." << endl << "Please enter 3 to not play, and end the program, like a loser XD.";
-	//First prompt
-
-	cin >> RunTest;
-
-	if (RunTest == 1){
+void RunTest(int Run){
+	if (Run == 1){
 		openFile = "Words.txt";
-
+		cout << "Thank you for playing" << endl;
 	}
-	if (RunTest == 2){
+	if (Run == 2){
 		cout << "Please enter a word." << endl;
 		cout << "Please enter a single ! to finish entering word." << endl;
 		while (user != "!"){
-		cin >> user;
+			cin >> user;
 		Dict.setWord(user);
-		}
-		openFile = "Words.txt";
-		}
-	if (RunTest == 3){
-		cout << "Have a nice day!" << endl;
-		exit(0);
-		}
-		GS.setWord(Dict.getRandWord(openFile));
-	while (Kplaying != 0){
-
-
-
 	}
-
-	return 0;
+		openFile = "Words.txt";
+	}
+	if (Run == 3){
+		cout << "Have a nice day!" << endl;
+	exit(0);
+	}
+	//GS.setWord(Dict.getRandWord(openFile));
+//while (Kplaying != 0){
 }
+//void GameRunTime()
+
+
+
+
+
+
+
 
 //pre (What you want/expect coming in)
 //post (What is output by the function)
 //desc (How the output is achieved)
 
+int main() {
+	int Run;
+	cout << "Welcome to the wild word guessing game!" << endl << "Please enter 1 to use the premade dictionary." << endl << "Please enter 2 to create your own dictionary." << endl << "Please enter 3 to not play, and end the program, like a loser XD.";
+		//First prompt
+	cin >> Run;
+	RunTest(Run);
+
+
+
+return 0;
+}
